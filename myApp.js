85a8a69bb7b1365ba5cd4700c7123f5317485af1
matt arias <mattwork07@gmail.com>
2022-02-16 18:06:47 +0000

    add findEditThenSave method

--- myApp.js
+++ myApp.js
@@ -55,8 +55,14 @@ const findPersonById = (personId, done) => {
 
 const findEditThenSave = (personId, done) => {
   const foodToAdd = "hamburger";
-
-  done(null /*, data*/);
+  Person.findById( personId , function(error, person){
+    if (error) return console.log(error)
+    person.favoriteFoods.push(foodToAdd);
+    person.save(function(err,updatedPerson){
+      if(err) return console.log(err)
+      done(null, updatedPerson);
+    })
+  }) 
 };
 
 const findAndUpdate = (personName, done) => {
